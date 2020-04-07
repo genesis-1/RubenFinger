25ace8291c5c65c7284d07a35510f571681952cf
patrick ntwari <patrick_ntwari@live.com>
2020-04-07 17:06:11 +0200

    PNT:Initial Commit

---
+++ RubenFinger.api/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using RubenFinger.api.Data;
+using System.IO;
+
+
+namespace DatingApp.API.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlite(connectionString);
+            return new DataContext(builder.Options);
+        }
+    }
+}
